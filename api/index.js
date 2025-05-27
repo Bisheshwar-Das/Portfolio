098d6ceaffbc61d5575f0ace34cf514106192588
Bisheshwar-Das <141819955+Bisheshwar-Das@users.noreply.github.com>
2025-05-27 23:38:05 +1000

    trying debugging

--- api/index.js
+++ api/index.js
@@ -1,4 +1,10 @@
 const serverless = require('serverless-http');
 const app = require('../app');
+app.get('/debug', (req, res) => {
+  db.all("SELECT name FROM sqlite_master WHERE type='table'", (err, rows) => {
+    if (err) return res.status(500).send('DB Error: ' + err.message);
+    res.send(`Connected! Tables: ${rows.map(r => r.name).join(', ')}`);
+  });
+});
 
 module.exports = serverless(app); 
